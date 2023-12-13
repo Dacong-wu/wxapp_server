5c2a4b686cbec9c1ee8c2ba84eaf2a533d577b47
Dacong-wu <2420390517@qq.com>
2023-12-13 14:19:42 +0800

    Update dev.js

--- dev.js
+++ dev.js
@@ -1,7 +1,7 @@
 const envInfo = {
   NODE_ENV: 'production', // 环境名称
   BASE_API: process.env.AZURE_BASE_API, // 基础域名
-  BASE_PORT: process.env.AZURE_BASE_PORT, // 监听端口
+  BASE_PORT: process.env.port, // 监听端口
   MYSQL_USER: process.env.AZURE_MYSQL_USER, // mysql用户名
   MYSQL_PASS: process.env.AZURE_MYSQL_PASSWORD, // mysql密码
   WX_APPID: process.env.AZURE_WX_APPID, // 小程序的appid
