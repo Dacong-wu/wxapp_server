c802a97a6dae6bcdc7c9e5df2113d2b35d0e70b3
Dacong-wu <2420390517@qq.com>
2024-04-07 15:55:37 +0800

    更新 mongoose 连接，解决用户名连接报错

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const { ContentSchema, MemorySchema, PublicSchema, UserSchema } = require('../utils/mongodbSchema')
 const { envData } = require('../env')
 
-mongoose.connect(envData.DB_LINK)
+mongoose.connect(envData.DB_LINK, { authSource: 'admin' })
 const ContentModel = mongoose.model('Content', ContentSchema)
 const MemoryModel = mongoose.model('Memory', MemorySchema)
 const PublicModel = mongoose.model('Public', PublicSchema)
