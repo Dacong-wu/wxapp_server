a8562d01ee3dd1553cee2c242a4e676181c85a7f
大葱 <2420390517@qq.com>
2024-04-14 15:17:34 +0800

    增加更新访问链接的功能

--- utils/bitiful.js
+++ utils/bitiful.js
@@ -1,5 +1,9 @@
 const { envData } = require('../env')
-const { S3Client, PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3')
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} = require('@aws-sdk/client-s3')
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
 
 const s3Client = new S3Client({
@@ -7,16 +11,16 @@ const s3Client = new S3Client({
   endpoint: 'https://s3.bitiful.net', //桶信息中的服务端点
   credentials: {
     accessKeyId: envData.S3_ACCESS_KEY_ID, //AccessKeyId
-    secretAccessKey: envData.S3_SECRET_ACCESS_KEY //AccessKey
-  }
+    secretAccessKey: envData.S3_SECRET_ACCESS_KEY, //AccessKey
+  },
 })
 
+//获取上传预签名链接
 function getPutObjectSignedUrl(Bucket, Key) {
   const putCmd = new PutObjectCommand({
     Bucket, //桶名称
-    Key //图片的key，avatar/ 就是把它放到avatar文件夹下
+    Key, //图片的key，avatar/ 就是把它放到avatar文件夹下
   })
-  //获取预签名链接
   return new Promise((resolve, rejects) => {
     getSignedUrl(s3Client, putCmd, { expiresIn: 3600 })
       .then(url => {
@@ -27,13 +31,12 @@ function getPutObjectSignedUrl(Bucket, Key) {
       })
   })
 }
-
+//获取访问预签名链接
 function getGetObjectSignedUrl(Bucket, Key) {
   const putCmd = new GetObjectCommand({
     Bucket, //桶名称
-    Key //图片的key，avatar/ 就是把它放到avatar文件夹下
+    Key, //图片的key，avatar/ 就是把它放到avatar文件夹下
   })
-  //获取访问签名链接
   return new Promise((resolve, rejects) => {
     getSignedUrl(s3Client, putCmd, { expiresIn: 3600 })
       .then(url => {
@@ -45,4 +48,21 @@ function getGetObjectSignedUrl(Bucket, Key) {
   })
 }
 
-module.exports = { getPutObjectSignedUrl, getGetObjectSignedUrl }
+async function updateSignedUrl(url) {
+  const regex = /\/\/([^\/]+)\.s3\.([^\/]+)\/([^?]+)/
+  const match = regex.exec(url)
+  if (match) {
+    const Bucket = match[1]
+    const Key = match[3]
+    let newUrl = await getGetObjectSignedUrl(Bucket, Key)
+    return newUrl
+  } else {
+    return false
+  }
+}
+
+module.exports = {
+  getPutObjectSignedUrl,
+  getGetObjectSignedUrl,
+  updateSignedUrl,
+}
