81c00c74b42fbb44584c73304ac2e064bd7055d6
大葱 <2420390517@qq.com>
2024-04-14 14:44:00 +0800

    增加获取访问图片的签名链接

--- api/upload/index.js
+++ api/upload/index.js
@@ -1,12 +1,40 @@
 const router = require('koa-router')()
-const { getPutObjectSignedUrl } = require('../../utils/bitiful')
+const {
+  getPutObjectSignedUrl,
+  getGetObjectSignedUrl,
+} = require('../../utils/bitiful')
 
 router.get('/signed-url', async ctx => {
   let { Bucket, Key } = ctx.query
   let url = await getPutObjectSignedUrl(Bucket, Key)
   ctx.body = {
     code: 1,
-    message: url
+    message: url,
+  }
+})
+
+router.get('/signed-url-v1', async ctx => {
+  let { Bucket, Key } = ctx.query
+  let url = await getPutObjectSignedUrl(Bucket, Key)
+  let host = 'https://bitiful.ll1025.cn/'
+  switch (Bucket) {
+    case 'wxapp-public':
+      host = 'https://bitiful.ll1025.cn/'
+      break
+  }
+  ctx.body = {
+    code: 1,
+    message: { host, url },
+  }
+})
+
+router.get('/signed-url-and-visit', async ctx => {
+  let { Bucket, Key } = ctx.query
+  let uploadUrl = await getPutObjectSignedUrl(Bucket, Key)
+  let visitUrl = await getGetObjectSignedUrl(Bucket, Key)
+  ctx.body = {
+    code: 1,
+    message: { uploadUrl, visitUrl },
   }
 })
 
